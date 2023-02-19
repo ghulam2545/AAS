b8441d65b0f6d2a51299f4be121b21084fc7c2d3
ghulam2545 <ghulam2545@gmail.com>
2023-02-20 02:21:22 +0530

    more UI and json inclusion

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const MongoDbSession = require("connect-mongodb-session")(session);
 const mainRoute = require("./router/mainRoute");
 const userRoute = require("./router/userRoute");
 const attendRoute = require("./router/attendRoute");
-const user_session = require("./controller/user_session");
+const reportRoute = require("./router/reportRoute");
 
 const app = express();
 
@@ -16,6 +16,7 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public/images"));
 app.use(express.static(__dirname + "/public/css"));
 app.use(express.static(__dirname + "/public/js"));
+app.use(express.static(__dirname + "/public"));
 // app.use(express.static(__dirname + "/public/time_table"));
 
 app.use(express.json());
@@ -42,6 +43,7 @@ app.use(
 app.use(mainRoute);
 app.use(userRoute);
 app.use(attendRoute);
+app.use(reportRoute);
 
 app.all("/", (req, res) => res.render("home"));
 
--- router/attendRoute.js
+++ router/attendRoute.js
@@ -3,4 +3,6 @@ const router = express.Router();
 
 router.get("/btech-1-year", (req, res) => res.render("attendance"));
 
+router.get("/list_student", (req, res) => res.render("./partials/_list"));
+
 module.exports = router;
